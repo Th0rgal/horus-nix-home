3f9a8d396fa91484a9b27261a32aaab7d51850d7
Thomas Marchand <me@thomas-marchand.com>
2019-12-16 13:54:10 +0100

    :sparkles: adds python with required packages

--- home.nix
+++ home.nix
@@ -1,36 +1,44 @@
 { config, pkgs, lib, ... }:
 
-{
-  nixpkgs.config.allowUnfree = true;
+with pkgs;
+let
+  my-python-packages = python-packages: with python-packages; [
+    pillow
+    requests
+  ]; 
+  python-with-my-packages = python3.withPackages my-python-packages;
+in
+  {
+    nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [    
-    gotop htop neofetch zip unzip xorg.xev scrot 
-    aria2 imagemagick feh (import ./alacritty-master.nix) # Till they fix the background_opacity bug
-    vscodium typora
-    zulu8 rustup gcc m4 gnumake binutils
-    nerdfonts powerline-fonts roboto siji (import ./termsyn.nix) source-code-pro dejavu_fonts noto-fonts-emoji
-    idea.idea-ultimate vlc
-  ];
+    home.packages = with pkgs; [    
+      gotop htop neofetch zip unzip xorg.xev scrot 
+      aria2 imagemagick feh (import ./alacritty-master.nix) # Till they fix the background_opacity bug
+      vscodium typora
+      python-with-my-packages zulu8 rustup gcc m4 gnumake binutils
+      nerdfonts powerline-fonts roboto siji (import ./termsyn.nix) source-code-pro dejavu_fonts noto-fonts-emoji
+      idea.idea-ultimate vlc
+    ];
 
-  programs = {
-    home-manager.enable = true;
-    command-not-found.enable = true;
-    firefox.enable = true;
+    programs = {
+      home-manager.enable = true;
+      command-not-found.enable = true;
+      firefox.enable = true;
 
-    # alacritty = import ./alacritty.nix { inherit pkgs; };
-    rofi = import ./rofi.nix { inherit pkgs; };
+      # alacritty = import ./alacritty.nix { inherit pkgs; };
+      rofi = import ./rofi.nix { inherit pkgs; };
 
-    git = {
-      enable = true;
-      userName = "Thomas Marchand";
-      userEmail = "thomas.marchand" + "@" + "tuta.io";
+      git = {
+        enable = true;
+        userName = "Thomas Marchand";
+        userEmail = "thomas.marchand" + "@" + "tuta.io";
+      };
     };
-  };
+      
+    xsession.windowManager.i3 = import ./i3.nix { inherit pkgs lib; };
     
-  xsession.windowManager.i3 = import ./i3.nix { inherit pkgs lib; };
-  
-  services = {
-    compton = import ./compton.nix {};
-    polybar = import ./polybar.nix { inherit pkgs; };
-  };
-}
+    services = {
+      compton = import ./compton.nix {};
+      polybar = import ./polybar.nix { inherit pkgs; };
+    };
+  }
