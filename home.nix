d1f2d5610aa2f54f187d51b3704eb476bded7d16
Thomas Marchand <me@thomas-marchand.com>
2020-01-04 17:19:48 +0100

    :sparkles: adds termius

--- home.nix
+++ home.nix
@@ -20,7 +20,7 @@ in
     home.packages = with pkgs; [    
       gotop htop neofetch zip unrar unzip xorg.xev scrot tree
       aria2 imagemagick feh (import ./alacritty-master.nix) # Till they fix the background_opacity bug
-      vscodium typora discord
+      vscodium typora discord (pkgs.callPackage ./termius.nix { })
       python-with-my-packages zulu8 rustup gcc m4 gnumake binutils
       powerline-fonts roboto siji (import ./termsyn.nix) source-code-pro dejavu_fonts noto-fonts-emoji
       idea.idea-ultimate vlc
