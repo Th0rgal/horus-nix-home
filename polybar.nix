b32eb594672450662b685409d7ff0bf19110e68d
Thomas Marchand <me@thomas-marchand.com>
2020-01-05 10:41:20 +0100

    :sparkles: updates ws-icon

--- polybar.nix
+++ polybar.nix
@@ -233,7 +233,7 @@ in
           ws-icon-3 = "4;ﭮ";
           ws-icon-4 = "5;";
           ws-icon-5 = "6;";
-          ws-icon-6 = "7;";
+          ws-icon-6 = "7;";
           ws-icon-7 = "8;";
           ws-icon-8 = "9;";
           ws-icon-9 = "10;";
