7b2c550edeb3c15b3929308f090e8f8fda3f2721
Thomas Marchand <me@thomas-marchand.com>
2020-08-07 21:03:35 +0200

    move keybase to system

--- home.nix
+++ home.nix
@@ -34,7 +34,7 @@ in
       # BLOCKCHAIN
       ledger-live-desktop
       # OFFICE
-      texlive.combined.scheme-medium wpsoffice typora brave keybase-gui
+      texlive.combined.scheme-medium wpsoffice typora brave
       # DEFAULT
       kotatogram-desktop discord vlc spotify gimp blueman wineWowPackages.stable obs-studio
       # GAMES
