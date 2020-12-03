89d2a60eb4d5a28ef5a6e69b82b6c383ab8d38c4
Thomas Marchand <me@thomas-marchand.com>
2020-12-03 14:57:48 +0100

    add p10k

--- home.nix
+++ home.nix
@@ -56,6 +56,23 @@ in
         userName = "Thomas Marchand";
         userEmail = "thomas.marchand" + "@" + "tuta.io";
       };
+
+
+      zsh = {
+        enable = true;
+        plugins = [
+          {
+            name = "powerlevel10k";
+            src = pkgs.zsh-powerlevel10k;
+            file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+          }
+          {
+            name = "powerlevel10k-config";
+            src = lib.cleanSource ./p10k-config;
+            file = "p10k.zsh";
+          }
+        ];
+      };
     };
 
     xsession.enable = true;
