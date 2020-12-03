5f30b62252e0c7b994519cb0bf95d39d18c07d64
Thomas Marchand <me@thomas-marchand.com>
2020-12-03 23:23:38 +0100

    add pading

--- alacritty.nix
+++ alacritty.nix
@@ -7,6 +7,10 @@
     settings = {
       window = {
         title = "Terminal";
+
+        padding = {
+          y = 5;
+        };
         dimensions = {
           lines = 75;
           columns = 100;
