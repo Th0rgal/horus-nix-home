7cdea2b930fcc70e1723d4dc8eca0b9ac328203a
Thomas Marchand <me@thomas-marchand.com>
2021-02-23 12:13:56 +0100

    update ledger

--- packages/ledgerlive.nix
+++ packages/ledgerlive.nix
@@ -1,13 +1,13 @@
-{ stdenv, fetchurl, makeDesktopItem, appimageTools, imagemagick }:
+{ lib, fetchurl, makeDesktopItem, appimageTools, imagemagick }:
 
 let
   pname = "ledger-live-desktop";
-  version = "2.20.0";
+  version = "2.21.3";
   name = "${pname}-${version}";
 
   src = fetchurl {
     url = "https://github.com/LedgerHQ/${pname}/releases/download/v${version}/${pname}-${version}-linux-x86_64.AppImage";
-    sha256 = "10gi29mcvs4d5flqycwid190pnlciznzbvg36250mxaxxs58rq7j";
+    sha256 = "11r6gwzg5qym7h40d8mrpw8c6zbdi534c2y7ghy2k0a4k3ybk8x1";
   };
 
   appimageContents = appimageTools.extractType2 {
@@ -26,7 +26,7 @@ in appimageTools.wrapType2 rec {
       --replace 'Exec=AppRun' 'Exec=${pname}'
   '';
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Wallet app for Ledger Nano S and Ledger Blue";
     homepage = "https://www.ledger.com/live";
     license = licenses.mit;
