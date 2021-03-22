19c89421d1034e661df6de3d1b6a9aaef2ca0a3a
Thomas Marchand <me@thomas-marchand.com>
2021-03-22 11:33:03 +0100

    install nixfmt extension within vscode

--- configs/vscode.nix
+++ configs/vscode.nix
@@ -13,6 +13,12 @@
 
       # Languages support
       {
+        name = "nixfmt-vscode";
+        publisher = "brettm12345";
+        version = "0.0.1";
+        sha256 = "07w35c69vk1l6vipnq3qfack36qcszqxn8j3v332bl0w6m02aa7k";
+      }
+      {
         name = "cmake-tools";
         publisher = "ms-vscode";
         version = "1.5.3";
