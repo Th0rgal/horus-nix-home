418cb36aecdd8d10deb6a482e22cb713991f3423
Thomas Marchand <me@thomas-marchand.com>
2021-05-02 09:23:15 +0200

    add zsh-autosuggestions

--- configs/main.nix
+++ configs/main.nix
@@ -27,20 +27,19 @@
       enable = true;
       plugins = [
         {
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-autosuggestions";
+            rev = "v0.6.4";
+            sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
+          };
+        }
+        {
           name = "powerlevel10k";
           src = pkgs.zsh-powerlevel10k;
           file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
         }
-        /* {
-             name = "fast-syntax-highlighting";
-             src = pkgs.fetchFromGitHub {
-               owner = "zdharma";
-               repo = "fast-syntax-highlighting";
-               rev = "v1.55";
-               sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
-             };
-           }
-        */
         {
           name = "powerlevel10k-config";
           src = lib.cleanSource ./p10k-config;
