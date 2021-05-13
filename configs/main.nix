8c0696acbc44c215435a10860dad56ef80927b0d
Thomas Marchand <me@thomas-marchand.com>
2021-05-13 20:46:52 +0200

    add fast-syntax-highlighting thanks to @SebTM

--- configs/main.nix
+++ configs/main.nix
@@ -25,6 +25,7 @@
 
     zsh = {
       enable = true;
+      initExtra = "export FAST_WORK_DIR=$HOME/.zsh/fsh/";
       plugins = [
         {
           name = "zsh-autosuggestions";
@@ -33,6 +34,15 @@
             repo = "zsh-autosuggestions";
             rev = "v0.6.4";
             sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
+          };
+        }
+        {
+          name = "fast-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zdharma";
+            repo = "fast-syntax-highlighting";
+            rev = "v1.55";
+            sha256 = "0h7f27gz586xxw7cc0wyiv3bx0x3qih2wwh05ad85bh2h834ar8d";
           };
         }
         {
