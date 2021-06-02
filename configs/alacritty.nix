af1a7c761ae9f42f2fe7ffc995af63116df74852
Thomas Marchand <me@thomas-marchand.com>
2021-06-02 21:57:35 +0200

    update colors

--- configs/alacritty.nix
+++ configs/alacritty.nix
@@ -38,8 +38,8 @@
           red = "0xFF301B";
           green = "0xA0E521";
           yellow = "0xFFC620";
-          blue = "0x178ad1";
-          magenta = "0x8763B8";
+          blue = "0x178AD1";
+          magenta = "0x9f7df5";
           cyan = "0x21DEEF";
           white = "0xEBEBEB";
         };
@@ -49,7 +49,7 @@
           green = "0xB8E466";
           yellow = "0xFFD750";
           blue = "0x1BA6FA";
-          magenta = "0xA578EA";
+          magenta = "0xB978EA";
           cyan = "0x73FBF1";
           white = "0xFEFEF8";
         };
