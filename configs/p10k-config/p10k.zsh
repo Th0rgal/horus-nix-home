3466d9d1b0fc7ded0570bd1e2694fead8d6498bf
Thomas Marchand <me@thomas-marchand.com>
2021-11-06 18:28:23 +0100

    toggle transient prompt

--- configs/p10k-config/p10k.zsh
+++ configs/p10k-config/p10k.zsh
@@ -1539,7 +1539,7 @@
   #   - always:   Trim down prompt when accepting a command line.
   #   - same-dir: Trim down prompt when accepting a command line unless this is the first command
   #               typed after changing current working directory.
-  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=off
+  typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=always
 
   # Instant prompt mode.
   #
